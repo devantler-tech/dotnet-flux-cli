3e786477a40e6c1f28ed4b56680eb38e7ab3a32e
Nikolai Emil Damm <ned@devantler.com>
2024-10-16 15:16:03 +0200

    Add new method for pushing artifacts to an OCI registry (#11)
    * Update Devantler.KubernetesProvisioner.Cluster.Kind to version 0.0.8 and add Devantler.ContainerEngineProvisioner.Docker package reference
    
    * Refactor Flux.cs to include a new method for pushing artifacts to an OCI registry
    
    * Add tests for Flux.PushArtifactAsync method

---
+++ Devantler.FluxCLI.Tests/FluxTests/PushArtifactAsyncTests.cs
@@ -0,0 +1,49 @@
+using Devantler.ContainerEngineProvisioner.Docker;
+
+namespace Devantler.FluxCLI.Tests.FluxTests;
+
+/// <summary>
+/// Tests for the <see cref="Flux.PushArtifactAsync(Uri, string, string, string, CancellationToken)"/> method.
+/// </summary>
+[Collection("Flux")]
+public class PushArtifactAsyncTests
+{
+  /// <summary>
+  /// Test to verify that the method pushes an artifact when the registry is configured.
+  /// </summary>
+  /// <returns></returns>
+  [Fact]
+  public async Task PushArtifactAsync_PushesArtifact_WhenRegistryIsConfigured()
+  {
+    // Arrange
+    var cancellationToken = new CancellationToken();
+    var dockerProvisioner = new DockerProvisioner();
+    await dockerProvisioner.CreateRegistryAsync("ksail-registry", 5555, cancellationToken);
+
+    // Act
+    string testFile = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+    await File.WriteAllTextAsync(testFile, "test");
+    var exception = await Record.ExceptionAsync(async () => await Flux.PushArtifactAsync(new Uri("oci://localhost:5555/test-artifact"), testFile, cancellationToken: cancellationToken).ConfigureAwait(false));
+
+    // Assert
+    Assert.Null(exception);
+
+    // Cleanup
+    File.Delete(testFile);
+    await dockerProvisioner.DeleteRegistryAsync("ksail-registry", cancellationToken);
+  }
+
+  /// <summary>
+  /// Test to verify that the method throws an <see cref="InvalidOperationException" /> when no registry is configured.
+  /// </summary>
+  /// <returns></returns>
+  [Fact]
+  public async Task PushArtifactAsync_ThrowsInvalidOperationException_WhenNoRegistryIsConfigured()
+  {
+    // Act
+    var exception = await Record.ExceptionAsync(async () => await Flux.PushArtifactAsync(new Uri("oci://localhost:5555/test-artifact"), "./Devantler.FluxCLI.Tests/assets", cancellationToken: new CancellationToken()).ConfigureAwait(false));
+
+    // Assert
+    _ = Assert.IsType<InvalidOperationException>(exception);
+  }
+}
